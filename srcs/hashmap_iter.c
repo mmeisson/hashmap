48cd9d3854df022eb05a024fb8d94ef1dca38b47
mmeisson <mmeisson@student.42.fr>
2019-03-24 00:16:50 +0100

    Add hashmap_iter

---
+++ srcs/hashmap_iter.c
@@ -0,0 +1,18 @@
+
+#include "_hashmap.h"
+
+void			hashmap_iter(s_hashmap *map, c_hashmap_iterator iter)
+{
+	if (map->entries != NULL)
+	{
+		for (size_t i = 0; i < map->nentries; i++)
+		{
+			s_entry		*entry = map->entries + i;
+
+			if (entry->key != NULL)
+			{
+				iter(entry->key, entry->content);
+			}
+		}
+	}
+}
